cf463c2d71d6525db49deb094fea5663a33d6b8e
Hakim Horairy <horairyhakim@gmail.com>
2025-05-15 12:32:34 +0200

    minimax

--- tictactoe_model.py
+++ tictactoe_model.py
@@ -1,5 +1,5 @@
 import numpy
-
+import random
 
 class TicTacToeModel:
 	def __init__(self):
@@ -23,19 +23,6 @@ class TicTacToeModel:
 		self.verfication_kernels.append(numpy.identity(3))
 		self.verfication_kernels.append(numpy.fliplr(numpy.identity(3)))
 
-		# self.verfication_kernels = [] # step 0 : générer un liste contenant les noyaux de vérification : 8 éléments
-		# up_row = [[1, 1, 1], [0, 0, 0], [0, 0, 0]]
-		# right_col = [[1, 0, 0], [1, 0, 0], [1, 0, 0]]
-		# self.verfication_kernels.append(numpy.array(up_row))
-		# self.verfication_kernels.append(numpy.flip(up_row))
-		# self.verfication_kernels.append(numpy.array([[0, 0, 0], [1, 1, 1], [0, 0, 0]]))
-
-		# self.verfication_kernels.append(numpy.array(right_col))
-		# self.verfication_kernels.append(numpy.flip(right_col))
-		# self.verfication_kernels.append(numpy.array([[0, 1, 0], [0, 1, 0], [0, 1, 0]]))
-
-		# self.verfication_kernels.append(numpy.identity(3))
-		# self.verfication_kernels.append(numpy.fliplr(numpy.identity(3)))
 
 
 	def human_move(self, row, col):
@@ -65,11 +52,88 @@ class TicTacToeModel:
 
 		return row, col
 
-	def get_optimal_move(self):
+
+	def get_empty_cells(self):
+		empty_cells = []
 		for row in range(0,3):
 			for col in range(0,3):
 				if self.grid[row, col] == 0:
-					return row, col
+					empty_cells.append((row, col))
+
+		return empty_cells
+
+
+
+
+	def get_optimal_move(self):
+		if numpy.all(self.grid==0):
+			return random.randint(0, 2), random.randint(0, 2)
+
+		best_score = -float("inf")
+		best_row, best_col = None, None
+		
+		for row, col in self.get_empty_cells():
+
+			self.grid[row, col] = 1
+			score = self.minimax_pruning(robot_turn=False)
+			if score > best_score:
+				best_score = score 
+				best_row, best_col = row, col
+			self.grid[row, col] = 0
+
+		return best_row, best_col
+
+
+	def minimax(self, robot_turn=False):
+		winner = self.check_winner()
+		if winner:
+			return winner
+
+		elif not self.check_game_progress():
+			return 0
+
+		# la partie back tracking qui parcours l'arbre des possibles
+		best_score = -float("inf") if robot_turn else float("inf")
+		for row, col in self.get_empty_cells():
+			self.grid[row, col] = 1 if robot_turn else -1
+			
+			score = self.minimax(not robot_turn)
+			best_score = max(score, best_score) if robot_turn else min(score, best_score)
+			
+			self.grid[row, col] = 0
+
+		return best_score
+
+
+
+	def minimax_pruning(self, robot_turn=False, alpha=-float("inf"), beta=float("inf")):
+		winner = self.check_winner()
+		if winner:
+			return winner
+
+		elif not self.check_game_progress():
+			return 0
+
+		# la partie back tracking qui parcours l'arbre des possibles
+		best_score = -float("inf") if robot_turn else float("inf")
+		for row, col in self.get_empty_cells():
+			self.grid[row, col] = 1 if robot_turn else -1
+			
+			score = self.minimax_pruning(not robot_turn, alpha, beta)
+
+			self.grid[row, col] = 0
+
+			if robot_turn:
+				best_score = max(score, best_score)
+				alpha = max(alpha, score)
+			else:
+				best_score = min(score, best_score)
+				beta = min(beta, score)
+
+			if beta <= alpha:
+				return best_score
+		
+		return best_score
 
 
 
